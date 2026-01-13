b3a842a3bd71e24db5de3216d0b8343ee73cc87f
Dharizza Espinach <dharizza.espinach@evolvingweb.com>
2026-01-13 17:27:53 +0000

    Add extra validation to check if ID is a number.

--- web/modules/custom/hello_world/src/Controller/HelloWorldController.php
+++ web/modules/custom/hello_world/src/Controller/HelloWorldController.php
@@ -36,6 +36,13 @@ final class HelloWorldController extends ControllerBase {
    * Builds the response for the hello_name_node route.
    */
   public function helloNameNode($name, $nid): array {
+    if (!is_numeric($nid)) {
+      return $build['content'] = [
+        '#type' => 'item',
+        '#markup' => $this->t('Hi @name! The ID should be a number.', ['@name' => $name]),
+      ];
+    }
+
     $node = Node::load($nid);
     // Print object.
     // dpm($node);
@@ -56,7 +63,7 @@ final class HelloWorldController extends ControllerBase {
       // Build link manually using Url and Link classes.
       $url = Url::fromRoute('entity.node.canonical', ['node' => $nid]);
       $link = Link::fromTextAndUrl('node link', $url);
-      
+
       $output = $this->t('Hello @name! The title of the node is @title.', [
         '@name' => $name,
         '@title' => $link->toString(),
