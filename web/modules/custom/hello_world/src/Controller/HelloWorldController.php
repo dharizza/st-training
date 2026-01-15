e915d57d038c60646724d51df3407e2303063131
Dharizza Espinach <dharizza.espinach@evolvingweb.com>
2026-01-15 18:06:42 +0000

    Print the name of the currently logged in user in /hello.

--- web/modules/custom/hello_world/src/Controller/HelloWorldController.php
+++ web/modules/custom/hello_world/src/Controller/HelloWorldController.php
@@ -20,8 +20,15 @@ final class HelloWorldController extends ControllerBase {
   public function hello($name = NULL): array {
     $output = $this->t('Hello World!');
 
-    if ($name) {
-      $output = $this->t('Hello @name!', ['@name' => $name]);
+    // If there is no name, we may want to use the name of the currently logged in user.
+    if (!$name) {
+      $user = \Drupal::currentUser();
+      // Check if the user is logged in and print its name. 
+      // If the user is anonymous then we continue to print Hello World.
+      if ($user->isAuthenticated()) {
+        $name = $user->getDisplayName();
+        $output = $this->t('Hello @name!', ['@name' => $name]);
+      }
     }
 
     $build['content'] = [
