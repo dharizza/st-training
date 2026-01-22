de2fcb540b128032333e4209588d5d956681ed54
Dharizza Espinach <dharizza.espinach@evolvingweb.com>
2026-01-22 20:02:30 +0000

    Populate file types automatically.

--- web/modules/custom/download_files/src/Form/DownloadFilesConfigForm.php
+++ web/modules/custom/download_files/src/Form/DownloadFilesConfigForm.php
@@ -32,14 +32,26 @@ final class DownloadFilesConfigForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state): array {
     $form['file_types'] = [
       '#type' => 'checkboxes',
-      '#options' => [
-        'application/pdf' => 'PDF',
-        'image/jpeg' => 'JPEG',
-      ],
+      '#options' => $this->getFileTypes(),
       '#title' => $this->t('File types to include in the download files form'),
       '#default_value' => $this->config('download_files.settings')->get('file_types'),
     ];
     return parent::buildForm($form, $form_state);
+  }
+
+  public function getFileTypes() {
+    $results = \Drupal::database()
+      ->select('file_managed', 'f')
+      ->distinct()
+      ->fields('f', ['filemime'])
+      ->execute()
+      ->fetchCol();
+
+    $types = [];
+    foreach ($results as $type) {
+      $types[$type] = $type;
+    }
+    return $types;
   }
 
   /**
