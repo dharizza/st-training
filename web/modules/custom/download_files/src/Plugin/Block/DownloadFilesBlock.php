167197e5c113ff4cab10b89f1051e89b62f89bff
Dharizza Espinach <dharizza.espinach@evolvingweb.com>
2026-01-23 01:56:25 +0000

    Added access callback to the block plugin.

--- web/modules/custom/download_files/src/Plugin/Block/DownloadFilesBlock.php
+++ web/modules/custom/download_files/src/Plugin/Block/DownloadFilesBlock.php
@@ -7,6 +7,8 @@ namespace Drupal\download_files\Plugin\Block;
 use Drupal\Core\Block\Attribute\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Access\AccessResult;
 
 /**
  * Provides a download files block block.
@@ -25,6 +27,14 @@ final class DownloadFilesBlock extends BlockBase {
     $form = \Drupal::formBuilder()->getForm('\Drupal\download_files\Form\DownloadFilesForm');
     $form['#title'] = $this->t('Get your files here!');
     return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function blockAccess(AccountInterface $account) {
+    $has_access = $account->hasPermission('download files');
+    return AccessResult::allowedIf($has_access);
   }
 
 }
