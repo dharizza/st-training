54831182c9075c38806dc45e0c350c66ce13a163
Dharizza Espinach <dharizza.espinach@evolvingweb.com>
2026-02-10 19:51:57 +0000

    Implement dependency injection on text transformations service.

--- web/modules/custom/amd_blocks/src/TextTransformations.php
+++ web/modules/custom/amd_blocks/src/TextTransformations.php
@@ -4,16 +4,37 @@ declare(strict_types=1);
 
 namespace Drupal\amd_blocks;
 
+use Drupal\Core\Logger\LoggerChannelFactory;
+use Drupal\Core\Logger\LoggerChannel;
+
 /**
  * Service that provides text transformations.
  */
 final class TextTransformations {
 
   /**
+   * Logger channel.
+   * 
+   * @var Drupal\Core\Logger\LoggerChannel
+   */
+  protected LoggerChannel $logger;
+
+  /**
+   * Construct an instance of TextTransformations
+   * 
+   * @param Drupal\Core\Logger\LoggerChannelFactory $loggerFactory
+   *   Instance of service logger.factory
+   */
+  public function __construct(LoggerChannelFactory $loggerFactory) {
+    $this->logger = $loggerFactory->get('amd_blocks');
+  }
+
+  /**
    * Reverse text received. Example: text to txet.
    */
   public function reverse($text): string {
-    \Drupal::logger('amd_blocks')->notice('The text ' . $text . ' was reversed.');
+    // \Drupal::logger('amd_blocks')->notice('The text ' . $text . ' was reversed.');
+    $this->logger->notice('The text ' . $text . ' was reversed.');
     return strrev($text);
   }
 
@@ -21,7 +42,8 @@ final class TextTransformations {
    * Uppercase all text received. Example: text to TEXT.
    */
   public function uppercase($text): string {
-    \Drupal::logger('amd_blocks')->notice('The text ' . $text . ' was transformed to UPPERCASE.');
+    // \Drupal::logger('amd_blocks')->notice('The text ' . $text . ' was transformed to UPPERCASE.');
+    $this->logger->notice('The text ' . $text . ' was transformed to UPPERCASE.');
     return strtoupper($text);
   }
 
@@ -29,7 +51,8 @@ final class TextTransformations {
    * Lowercase all the text received. TEXT to text.
    */
   public function lowercase($text): string {
-    \Drupal::logger('amd_blocks')->notice('The text ' . $text . ' was transformed to lowercase.');
+    // \Drupal::logger('amd_blocks')->notice('The text ' . $text . ' was transformed to lowercase.');
+    $this->logger->notice('The text ' . $text . ' was transformed to lowercase.');
     return strtolower($text);
   }
 
